3b21af43670a8472e9dea45654f096ea973cc515
kentsay <naturen@gmail.com>
2017-03-10 09:47:38 +0100

    add controller endpoint test

---
+++ src/test/java/de/scout24/controller/HtmlAnalyzerControllerTest.java
@@ -0,0 +1,31 @@
+package de.scout24.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HtmlAnalyzerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void A_postLocation() throws Exception {
+        MvcResult result = this.mockMvc.perform(post("/analyzer")
+                .content("https://jsoup.org/cookbook/input/load-document-from-file")
+        )
+                .andExpect(status().isOk()).andReturn();
+
+    }
+}
